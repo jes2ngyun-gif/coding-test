3bac4bb364d3590a42cd67a08b7f2a3853922068
최정윤 <jes2ngyun@gmail.com>
2026-06-08 11:38:46 +0900

    docs: add comments to p120806

--- src/algo/lv0/p120806/Solution.java
+++ src/algo/lv0/p120806/Solution.java
@@ -4,7 +4,7 @@ package algo.lv0.p120806;
 public class Solution {
 
     public int solution(int num1, int num2) {
-        double a = (double) num1 / num2 * 1000;
-        return (int) a;
+        double a = (double) num1 / num2 * 1000;                  // num1을 double형으로 바꿔 소수점 계산이 가능하도록 만듬.
+        return (int) a;                                          // 결과를 int형으로 변환하여 반환한다.
     }
 }
