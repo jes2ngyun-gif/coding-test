c1d4f651fcc5fc2225ca71d815e68b094031f133
최정윤 <jes2ngyun@gmail.com>
2026-08-14 09:06:28 +0900

    코드카타

---
+++ src/algo/lv1/p120836/Solution.java
@@ -0,0 +1,16 @@
+package algo.lv1.p120836;
+
+public class Solution {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
